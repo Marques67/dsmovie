931428232673cb736a6e329c154a89d77866d76c
Lucas Marques <lucasmarquesff@hotmail.com>
2022-01-14 15:10:45 -0300

    Props

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -1,16 +1,14 @@
 import MovieScore from "components/MovieScore";
+import { type } from "os";
 import { Link } from "react-router-dom";
+import { Movie } from "types/movie";
 
 
-function MovieCard() {
+type Props = {
+    movie: Movie;
+}
 
-    const movie = {
-        id: 1,
-        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTC9_OH-rGiK3DNdgcFSvik8Z8h0n7TVQ1KJg&usqp=CAU",
-        title: "Batman: O Cavaleiro das Trevas",
-        count: 2,
-        score: 4.5
-    };
+function MovieCard({ movie } : Props) {
 
     return (
         <div>
