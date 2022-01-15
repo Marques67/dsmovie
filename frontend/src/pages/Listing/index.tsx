bd5960e697648deff35819d1593b3120ba73fce1
Lucas Marques <lucasmarquesff@hotmail.com>
2022-01-15 10:26:44 -0300

    Pagination

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -29,17 +29,13 @@ function Listing() {
             });
     }, [pageNumber]);
 
-    const movie = {
-        id: 1,
-        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTC9_OH-rGiK3DNdgcFSvik8Z8h0n7TVQ1KJg&usqp=CAU",
-        title: "Batman: O Cavaleiro das Trevas",
-        count: 2,
-        score: 4.5
-    };
+    const handlePageChange = (newPageNumber : number) => {
+        setPageNumber(newPageNumber);
+    }
 
     return (
         <>
-            <Pagination />
+            <Pagination page={page} onChange={handlePageChange}/>
             <div className="container">
                 <div className="row">
                     {page.content.map(movie => (
